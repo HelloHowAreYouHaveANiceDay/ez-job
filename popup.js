14d8109f699770315a7ac058ebf34b91917a1a2a
liboli.lilibo@gmail.com <liboli.lilibo@gmail.com>
2023-05-08 21:00:58 -0700

    added gitignore, start work on API calls

--- popup.js
+++ popup.js
@@ -1,7 +1,10 @@
-const getTabText = async () => {
+
+
+// get text from the currently active tab
+const getTabText = async (tabId) => {
     const tabText = await chrome.scripting.executeScript({
         target: {
-            tabId: await getActiveTabId()
+            tabId,
         },
         func: () => document.documentElement.innerText
     })
@@ -9,9 +12,17 @@ const getTabText = async () => {
    console.log(result)
 }
 
+// ge the id of the currently active tab 
 const getActiveTabId = async () => {
     const tabs = await chrome.tabs.query({active: true, currentWindow: true})
     return tabs[0].id
 }
 
-getTabText()
+
+const getAirtableSchema = async () => {
+    fetch('http://www.example.com?par=0').then(r => r.text()).then(result => {
+        // Result now contains the response text, do what you want...
+    })
+}
+
+getTabText(getActiveTabId())
